7ab2e50f096629f60893ab31041b44debfda2931
김성령 <107569343+MTsauRus@users.noreply.github.com>
2025-10-29 23:36:35 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ MySQL/프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,11 +1,18 @@
-select distinct
+select 
     B.title, 
-    B.board_id,
-    R.reply_id,
-    R.writer_id,
-    R.contents,
-    date_format(R.created_date, '%Y-%m-%d') as created_date
-from used_goods_board, used_goods_reply as R
-join used_goods_board as B on B.board_id = R.board_id
-where B.created_date like '2022-10%'
-order by R.created_date, B.title;
+    B.board_id, 
+    R.reply_id, 
+    R.writer_id, 
+    R.contents, 
+    date_format(R.created_date, '%Y-%m-%d') as created_date 
+from
+    used_goods_board as B
+join
+    used_goods_reply as R
+    on B.board_id = R.board_id
+where
+    date_format(B.created_date, '%Y-%m') = '2022-10'
+order by
+    R.created_date asc, 
+    B.title asc
+;
