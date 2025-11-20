d0dac365e55b69bdd94ed574cbe70b83a811941f
김성령 <107569343+MTsauRus@users.noreply.github.com>
2025-11-20 15:43:04 +0900

    [Gold IV] Title: 알고스팟, Time: 120 ms, Memory: 114340 KB -BaekjoonHub

--- PyPy3/백준/Gold/1261. 알고스팟/알고스팟.py
+++ PyPy3/백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,30 +1,31 @@
-### 1261. 알고스팟(G4)
-### 다익스트라
+### 1261. 알고스팟 (G4)
+### 0-1 BFS
 import sys
 input = sys.stdin.readline
-from heapq import heappush, heappop
+from collections import deque
 
 C, R = map(int, input().split())
 G = []
 for i in range(R):
     G.append(list(map(int, input().strip())))
-
-dist = [[float('inf') for _ in range(C)] for _ in range(R)]
-queue = [[0, 0, 0]] # dist, x, y
+    
+queue = deque([[0, 0]])
+dist = [[-1 for _ in range(C)] for _ in range(R)]
 dist[0][0] = 0
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
+
 while queue:
-    now_dist, x, y = heappop(queue)
-    if now_dist > dist[x][y]:
-        continue
+    x, y = queue.popleft()
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0 <= nx < R and 0 <= ny < C:
-            next_dist = now_dist + G[nx][ny]
-            if next_dist < dist[nx][ny]:
-                dist[nx][ny] = next_dist
-                heappush(queue, [next_dist, nx, ny])
-
+        if 0 <= nx < R and 0 <= ny < C and dist[nx][ny] == -1:
+            if G[nx][ny] == 0:
+                queue.appendleft([nx, ny])
+                dist[nx][ny] = dist[x][y]
+            else: # G[nx][ny] == 1
+                queue.append([nx, ny])
+                dist[nx][ny] = dist[x][y] + 1
+                
 print(dist[R-1][C-1])
