37507c2403b12fad40b0028e0e885fe1437751a1
김성령 <107569343+MTsauRus@users.noreply.github.com>
2025-11-28 17:10:27 +0900

    [Gold IV] Title: 뱀, Time: 116 ms, Memory: 113500 KB -BaekjoonHub

--- PyPy3/백준/Gold/3190. 뱀/뱀.py
+++ PyPy3/백준/Gold/3190. 뱀/뱀.py
@@ -1,4 +1,4 @@
-### 3190. 뱀 (G4)
+### 3190. 뱀 (G4) - 리팩토링
 ### 시뮬레이션, 구현
 import sys
 input = sys.stdin.readline
@@ -6,93 +6,56 @@ from collections import deque
 
 N = int(input())
 K = int(input())
-apples = []
+board = [[0] * N for _ in range(N)]
+
 for _ in range(K):
     r, c = map(int, input().split())
-    apples.append([r-1, c-1])
-moves = []
+    board[r-1][c-1] = 2 # 사과: 2
+    
 L = int(input())
+turns = {} # 방향 정보 -> dict
 for _ in range(L):
-    sec, turn = input().split()
-    sec = int(sec)
-    moves.append([sec, turn])
-
-moves.append([999, 'END']) # 임의의 마지막 이동 노드
-
-moves = deque(moves)
-G = [[-1 for _ in range(N)] for _ in range(N)]
-G[0][0] = 0
-## -1: 빈칸, -2: 사과, 0 이상: 뱀
-for r, c in apples:
-    G[r][c] = -2
+    x, c = input().split()
+    turns[int(x)] = c
     
 dr = [0, 1, 0, -1]
-dc = [1, 0, -1, 0] # -> ㅜ <- ㅗ
-ans = 0
-head = [0, 0]
-tail = [0, 0]
-next_node = [0, 1]
-now_direction = 0
-next_tmp = moves.popleft()
-next_sec, next_turn = next_tmp[0], next_tmp[1]
+dc = [1, 0, -1, 0]
 
-def check_safe(next_node): # r, c는 다음 이동 칸
-    r, c = next_node[0], next_node[1]
+def sol():
+    x, y = 0, 0 # 현재 뱀의 머리
+    board[0][0] = 1 # 현재 뱀의 몸통 표시
+    direction = 0 # 초기 방향 ->
+    time = 0 # 현재 경과 시간
     
-    # 칸을 벗어났거나, 뱀이라면
-    if (not (0 <= r < N)) or (not (0 <= c < N)) or G[r][c] >= 0:
-        return False # False: 종료
-    return True
+    snake = deque([(0, 0)]) 
     
-def sol():    
-    global ans, head, tail, next_node, next_sec, next_turn, now_direction
     while True:
-        if ans == next_sec: # 방향 전환 시기가 오면
-            if next_turn == 'L':
-                now_direction = (now_direction-1)%4
+        time += 1
+        
+        nx = x + dr[direction]
+        ny = y + dc[direction]
+        
+        # 다음 방향이 벽이거나 자기 몸일 때
+        if not (0 <= nx < N and 0 <= ny < N) or board[nx][ny] == 1:
+            return time
+        
+        # 이동
+        if board[nx][ny] == 2: # 사과가 있다면
+            board[nx][ny] = 1 # 머리 이동
+            snake.append((nx, ny)) # 뱀에 새 머리 append.
+        else: # 사과가 없다면
+            board[nx][ny] = 1 # 머리 이동
+            snake.append((nx, ny)) # 뱀에 새 머리 append. 여기까지 같음.
+            
+            tx, ty = snake.popleft() # 꼬리 제거
+            board[tx][ty] = 0 # 보드에서 꼬리 제거
+            
+        x, y = nx, ny # 머리 갱신
+        
+        if time in turns: # 현재 시간이 turns의 key에 있다면
+            if turns[time] == 'D':
+                direction = (direction + 1) % 4
             else:
-                now_direction = (now_direction+1)%4
-            
-            next_sec, next_turn = moves.popleft()
-        
-        next_node = [head[0] + dr[now_direction], head[1] + dc[now_direction]]    
-        if not check_safe(next_node):
-                return ans+1 # 게임 종료
-            
-        move()
-        ans += 1
-        #debug_print()
-def move():
-    global head, tail, next_node
-    nr, nc = next_node[0], next_node[1] # 다음
-    cr, cc = head[0], head[1] # 현재
-    
-    if G[nr][nc] == -2: # 다음칸이 사과라면
-        G[nr][nc] = G[cr][cc] + 1 # 현재 노드 + 1을 다음 노드에 저장, 추후 모듈러 처리
-        head = next_node # 다음 노드로 머리 이동
-        return
-    
-    else: # 사과가 없었다면
-        # 머리 이동은 동일
-        G[nr][nc] = G[cr][cc] + 1 # 현재 노드 + 1을 다음 노드에 저장, 추후 모듈러 처리
-        head = next_node # 다음 노드로 머리 이동
-        tail_r, tail_c = tail[0], tail[1]
-        for i in range(4):
-            next_r = tail_r + dr[i]
-            next_c = tail_c + dc[i]
-            # 꼬리 바로 다음 칸을 찾으면
-            if 0 <= next_r < N and 0 <= next_c < N and G[next_r][next_c] == G[tail_r][tail_c] + 1:
-                G[tail_r][tail_c] = -1 # 원래 꼬리칸을 빈칸으로 만들고
-                tail = [next_r, next_c] # 다음 칸을 꼬리로 업데이트
-                break # 4짜리 for문에서 탈출
-
-def debug_print():
-    print(ans)
-    for next in G:
-        print(next)
-    
-    print()
-    print()
-        
-#debug_print()
+                direction = (direction - 1) % 4
+                
 print(sol())
