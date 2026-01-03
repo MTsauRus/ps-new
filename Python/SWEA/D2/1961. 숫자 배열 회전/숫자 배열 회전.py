829ab22e5935e268fdfbbea07359afb12f1e1327
김성령 <107569343+MTsauRus@users.noreply.github.com>
2026-01-03 20:26:54 +0900

    [D2] Title: 숫자 배열 회전, Time: 55 ms, Memory: 52,352 KB -BaekjoonHub

--- Python/SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ Python/SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,33 +1,50 @@
-T = int(input())
-for t in range(1, T+1):
-    N = int(input())
-    arr = []
-    for i in range(N):
-        arr.append(list(input().split()))
-    
-    ansA = [[0 for _ in range(N)] for _ in range(N)]
-    ansB = [[0 for _ in range(N)] for _ in range(N)]
-    ansC = [[0 for _ in range(N)] for _ in range(N)]
-    
-    for i in range(N):
-        for j in range(N):
-            ansA[i][j] = arr[N-1-j][i]
-            ansB[i][j] = arr[N-1-i][N-1-j]
-            ansC[i][j] = arr[j][N-1-i]
-            
-    ans = []
-    for i in range(N):
-        tmp = ""
-        for j in range(N):
-            tmp += (ansA[i][j])
-        tmp += (" ")
-        for j in range(N):
-            tmp += (ansB[i][j])
-        tmp += (" ")
-        for j in range(N):
-            tmp += (ansC[i][j])
-        ans.append(tmp)
-    
-    print(f'#{t}')
-    for i in range(N):
-        print(ans[i])
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int T = nextInt();
+        
+        for (int t = 1; t <= T; t++) {
+            int N = nextInt();
+            int[][] arr = new int[N][N]; 
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = nextInt();
+                }
+            }
+
+            sb.append("#").append(t).append("\n");
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    sb.append(arr[N - 1 - j][i]);
+                } sb.append(" ");
+                for (int j = 0; j < N; j++) {
+                    sb.append(arr[N - 1 - i][N - 1 - j]);
+                } sb.append(" ");
+                for (int j = 0; j < N; j++) {
+                    sb.append(arr[j][N - 1 - i]);
+                } sb.append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            String line = br.readLine();
+            if (line == null) return null;
+            st = new StringTokenizer(line);
+        } return st.nextToken();
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+}
